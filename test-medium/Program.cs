81ccf253d978ddd6a7302a08d2d39d53a3a959ac
Luca La Malfa <luca.lamalfa@kampaay.com>
2022-06-03 12:53:50 +0200

    feat(scrapingData) create dll for extract and Parse correctly the data from URL

--- test-medium/Program.cs
+++ test-medium/Program.cs
@@ -13,7 +13,7 @@ namespace test_medium
 			var response = scrapingData.CallUrl(Addresses.ListOfComposers).Result;
 			var stringComposerList = scrapingData.ParseHtml(response);
 
-			//implement logic for filter properly the stringComposerList
+			//implement logic for filter properly the stringComposerList in a new class.
 
 			// return the result in desktop folder as csv file.
 			scrapingData.WriteToCsv(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), stringComposerList);
